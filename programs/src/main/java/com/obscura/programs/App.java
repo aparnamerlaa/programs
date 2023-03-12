7780ab1261fb4870d92a3575e98809de12b7aec3
aparnamerlaa <125823007+aparnamerlaa@users.noreply.github.com>
2023-03-12 21:15:06 +0530

    Add files via upload

---
+++ programs/src/main/java/com/obscura/programs/App.java
@@ -0,0 +1,13 @@
+package com.obscura.programs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
